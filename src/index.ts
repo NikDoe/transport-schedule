b45e632a72d9704b76546feec44621c9dee5691c
NikDoe <nikd0e@yandex.ru>
2022-08-08 16:35:13 +0300

    feature(schedule): basic setup express
    - use read json middleware;
    - create get request;
    - create post request;
    - listening port setup;

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('bye there');
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT: number = 9001;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+	res.send('hi there :)');
+});
+
+app.post('/auth/login', (req: Request, res: Response) => {
+	console.log(req.body);
+	res.json({
+		success: true,
+	});
+});
+
+app.listen(PORT, () => {
+	console.log(`listening on port ${PORT}`);
+});
