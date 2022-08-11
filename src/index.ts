a9e8652002450126af86df56ad9e1ee9be2ebde4
NikDoe <nikd0e@yandex.ru>
2022-08-11 17:15:22 +0300

    feature(schedule): add router logic

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,13 @@
 import 'dotenv/config';
-import express, { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
+import express from 'express';
 import sequelize from './db';
+import router from './router/router';
 
 const app = express();
 const PORT: string | number = process.env.PORT || 9001;
 
 app.use(express.json());
-
-app.get('/', (req: Request, res: Response) => {
-	res.send('hi there :)');
-});
-
-app.post('/auth/login', (req: Request, res: Response) => {
-	const token = jwt.sign(
-		{
-			email: req.body.email,
-			nikName: 'NikDoe',
-		},
-		'qwe3226265qwe',
-	);
-	res.json({
-		success: true,
-		token,
-	});
-});
+app.use('/api', router);
 
 const start = async () => {
 	try {
@@ -40,5 +23,3 @@ const start = async () => {
 };
 
 start();
-
-console.log(2);
